59e60c50fb54ed618b092a37ec40b478051bacbf
Débora <debora.almeida.de.mello@gmail.com>
2021-11-29 11:48:48 -0300

    advinhacao.py: o código foi todo definido em def's, inclusive o próprio arquivo. Agora o jogo também possúi frases persolanizadas de vitória e derrota.

--- advinhacao.py
+++ advinhacao.py
@@ -1,56 +1,116 @@
 import random
 
-print("**********************************")
-print("*Bem vindo ao jogo de advinhação!*")
-print("**********************************")
 
-chute = 0
-numero_secreto = random.randrange(1,101)
+def jogar():
 
-jogadas = 0
-dificuldade = 0
-pontos = 1000
+    abertura()
+    numero_secreto = gerando_numero_secreto()
 
-print("Você precisa advinhar um número de 1 à 100!")
+    chute = 0
+    pontos = 1000
 
-print("Escolha a dificuldade:")
-dificuldade = int(input("(1) - Fácil  (2) - Médio  (3) - Difícil "))
-if dificuldade == 1:
-    print("Facíl foi selecionado!\n")
-    jogadas = 20
-elif dificuldade == 2:
-    print("Médio foi selecionado!\n")
-    jogadas = 10
-else:
-    print("Difícil foi selecionado!\n")
-    jogadas = 5
+    jogadas = escolher_dificuldade()
 
-for rodada in range(1, jogadas + 1):
-    rodada = jogadas - rodada
+    print("Você precisa advinhar um número de 1 à 100!")
 
-    chute = int(input("Qual o seu chute? "))
+    venceu = resultado(jogadas, numero_secreto, pontos)
 
-    acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
-    perdeu = rodada == 0
+    if (venceu == True):
+        menssagem_vencedora()
+    else:
+        menssagem_perdedora(numero_secreto)
 
-    if (acertou):
-        print("Você acertou")
-        print("Sua pontuação foi {}".format(pontos))
-        break
-    elif (maior):
-        print("Seu chute foi maior que o número secreto!")
-        pontos = pontos - abs(chute - numero_secreto)
-    elif (menor):
-        print("Seu chute foi menor do que o número secreto!")
-        pontos = pontos - abs(chute - numero_secreto)
+    print("Fim de jogo!")
 
-    if (not perdeu):
-        print("Você ainda tem {} chances de {}\n".format(rodada, jogadas))
-    elif (perdeu):
-        print("Você perdeu!")
-        print("Sua pontuação foi {}".format(pontos))
-        print("O número secreto era {}".format(numero_secreto))
 
-print("Fim de jogo!")
+def abertura():
+    print("**********************************")
+    print("*Bem vindo ao jogo de advinhação!*")
+    print("**********************************")
+
+
+def gerando_numero_secreto():
+    return random.randrange(1,101)
+
+
+def escolher_dificuldade():
+    print("Escolha a dificuldade:")
+    dificuldade = int(input("(1) - Fácil  (2) - Médio  (3) - Difícil "))
+    if dificuldade == 1:
+        print("Facíl foi selecionado!\n")
+        jogadas = 20
+    elif dificuldade == 2:
+        print("Médio foi selecionado!\n")
+        jogadas = 10
+    else:
+        print("Difícil foi selecionado!\n")
+        jogadas = 5
+    return jogadas
+
+
+def resultado(jogadas, numero_secreto, pontos):
+
+    for rodada in range(1, jogadas + 1):
+        rodada = jogadas - rodada
+
+        chute = int(input("Qual o seu chute? "))
+
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+        perdeu = rodada == 0
+
+        if (acertou):
+            print("Sua pontuação foi {}".format(pontos))
+            return True
+        elif (maior):
+            print("Seu chute foi maior que o número secreto!")
+            pontos = pontos - abs(chute - numero_secreto)
+        elif (menor):
+            print("Seu chute foi menor do que o número secreto!")
+            pontos = pontos - abs(chute - numero_secreto)
+
+        if (not perdeu):
+            print("Você ainda tem {} chances de {}\n".format(rodada, jogadas))
+        elif (perdeu):
+            print("Sua pontuação foi {}".format(pontos))
+            return False
+
+
+def menssagem_vencedora():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def menssagem_perdedora(numero_secreto):
+    print("Puxa, você perdeu!")
+    print("O número secreto era {}".format(numero_secreto))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+if (__name__ == "__main__"):
+    jogar()
