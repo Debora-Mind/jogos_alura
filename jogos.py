17bb8b12d2ec6e21f98d5edb4382c9f8c2870c8d
Débora <debora.almeida.de.mello@gmail.com>
2021-11-29 16:34:50 -0300

    jogos.py: o jogo forca foi adicionado e agora pode ser acessado através de jogos.py

--- jogos.py
+++ jogos.py
@@ -1,14 +1,18 @@
 import advinhacao
+import forca
 
 def escolher_jogo():
     print("*********************")
     print("*Escolha o seu jogo!*")
     print("*********************")
 
-    escolha = int(input("(1) - Advinhação\n"))
+    escolha = int(input("(1) - Advinhação \n(2) - Forca \n"))
 
     if (escolha == 1):
         advinhacao.jogar()
+    elif (escolha == 2):
+        forca.jogar()
+
 
 if (__name__ == "__main__"):
     escolher_jogo()
