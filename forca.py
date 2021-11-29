c2aabe5ccd8dc77913a1bc51832505f3deb8d5a3
Débora <debora.almeida.de.mello@gmail.com>
2021-11-29 14:01:52 -0300

    forca.py: foi melhorada a visualização do usuário sobre a quantidade de letras faltantes e quando ele acerta é indicada a posição da letra

--- forca.py
+++ forca.py
@@ -1,17 +1,34 @@
-print("**********************************")
-print("***Bem vindo ao jogo da forca!****")
-print("**********************************")
+def jogar():
 
-palavra_secreta = "banana"
-tentativas = 10
-chute = ""
+    abertura()
 
-for rodada in range(tentativas):
-    chute = input("Digite uma letra: ")
+    palavra_secreta = "banana"
+    palavras = ["_" for letras in palavra_secreta]
+    print(palavras)
 
-    if (chute in palavra_secreta):
-        print(chute)
-    else:
-        print("Errou...")
+    enforcado = False
+    acertou = False
+    erros = 0
 
-print("Fim de jogo.")
+    while (not enforcado or not acertou):
+        chute = input("Digite uma letra")
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (letra == chute):
+                    palavras[index] = letra
+                index += 1
+        else:
+            erros += 1
+        print (palavras)
+
+    print("Fim de jogo.")
+
+def abertura():
+    print("**********************************")
+    print("***Bem vindo ao jogo da forca!****")
+    print("**********************************")
+
+if (__name__ == "__main__"):
+    jogar()
