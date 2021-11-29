14508e5a6f0e0844a9d1688d4ee8b14288ac6b74
Débora <debora.almeida.de.mello@gmail.com>
2021-11-29 12:35:11 -0300

    Arquivo .gitignore adicionado. jogos.py: foi adicionada opção do jogo advinhação

--- jogos.py
+++ jogos.py
@@ -1,3 +1,14 @@
-print("*********************")
-print("*Escolha o seu jogo!*")
-print("*********************")
+import advinhacao
+
+def escolher_jogo():
+    print("*********************")
+    print("*Escolha o seu jogo!*")
+    print("*********************")
+
+    escolha = int(input("(1) - Advinhação\n"))
+
+    if (escolha == 1):
+        advinhacao.jogar()
+
+if (__name__ == "__main__"):
+    escolher_jogo()
