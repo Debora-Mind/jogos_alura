32f4af6f5c8101f57c11268f4b6611cb81b232bf
Débora <debora.almeida.de.mello@gmail.com>
2021-11-29 10:34:50 -0300

    advinhacao.py: é mostrado na tela quando o usuario perde qual era o número secreto, também foi atribuído níveis de dificuldade ao jogo

--- advinhacao.py
+++ advinhacao.py
@@ -5,11 +5,22 @@ print("**********************************")
 chute = 0
 numero_secreto = 54
 
-jogadas = 5
+jogadas = 0
+dificuldade = 0
 
 print("Você precisa advinhar um número de 1 à 100!")
 
+print("Escolha a dificuldade:")
+dificuldade = int(input("(1) - Fácil  (2) - Médio  (3) - Difícil "))
+if dificuldade == 1:
+    jogadas = 20
+elif dificuldade == 2:
+    jogadas = 10
+else:
+    jogadas = 5
+
 while (jogadas != 0):
+
     chute = int(input("Qual o seu chute? "))
 
     if (chute == numero_secreto):
@@ -20,7 +31,12 @@ while (jogadas != 0):
         jogadas -= 1
     else:
         print("Seu chute foi menor do que o número secreto!")
+        jogadas -= 1
 
-    print("Você ainda tem {} chances".format(jogadas))
+    if (jogadas != 0):
+        print("Você ainda tem {} chances".format(jogadas))
+    elif (jogadas == 0):
+        print("Você perdeu!")
+        print("O número secreto era {}".format(numero_secreto))
 
 print("Fim de jogo!")
