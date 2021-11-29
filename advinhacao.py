b8b5a7ebf9f3e4bd2b2b56c6d542dfe1a3a0a90b
Débora <debora.almeida.de.mello@gmail.com>
2021-11-29 10:15:59 -0300

    advinhacao.py: while não tem mais um loop infinito, é possível vencer ou perder

--- advinhacao.py
+++ advinhacao.py
@@ -5,10 +5,18 @@ print("**********************************")
 chute = 0
 numero_secreto = 54
 
-print("Você precisa advinhar um número de 1 à 100!")
-chute = input("Qual o seu chute?")
+jogadas = 5
 
-if (chute == numero_secreto):
-    print("Você acertou")
-else:
-    print("Você errou")
+print("Você precisa advinhar um número de 1 à 100!")
+
+while (jogadas != 0):
+    chute = int(input("Qual o seu chute?"))
+
+    if (chute == numero_secreto):
+        print("Você acertou")
+        break
+    else:
+        print("Você errou")
+        jogadas -= 1
+
+print("Fim de jogo!")
